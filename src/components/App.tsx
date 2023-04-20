b22e49aa912ca8a926b0b3c9cf5afc2b537113d4
Hemal99 <h.githmin@gmail.com>
2023-04-20 14:31:44 +0530

    feature : context API configured

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useReducer, useEffect } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { lightBlue } from "@mui/material/colors";
-
+import { ITasksState } from "../interfaces/index";
+import { ActionTypes } from "../consts/index";
 import AppRoutes from "../routes/AppRoutes";
+import taskReducer from "../reducers/TaskReducer";
+import Context from "../context/tasksContext";
 
 // Generate Custom Global theme by Material UI
 
@@ -23,9 +26,27 @@ const theme = createTheme({
 });
 
 export default function App() {
+  const [state, dispatch] = useReducer(taskReducer, []);
+
+  useEffect(() => {
+    const taskJSON = localStorage.getItem("tasks");
+    const tasks: ITasksState = taskJSON !== null ? JSON.parse(taskJSON) : [];
+
+    if (tasks) {
+      dispatch({ type: ActionTypes.Fetch, tasks });
+    }
+  }, []);
+
+  // every change in our state will rewrite in our local sotrage
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(state));
+  }, [state]);
+
   return (
-    <ThemeProvider theme={theme}>
-      <AppRoutes />
-    </ThemeProvider>
+    <Context.Provider value={{ state, dispatch }}>
+      <ThemeProvider theme={theme}>
+        <AppRoutes />
+      </ThemeProvider>
+    </Context.Provider>
   );
 }
---
+++ src/selectors/filterTasks.tsx
@@ -0,0 +1,25 @@
+import { ITasksState, FilterTask } from "../interfaces/index";
+
+// this function helps to filter tasks in *TasksList* Component
+// this finction accepts a state as tasks, filter them and return a new state
+
+const filterTasks = (tasks: ITasksState, filter: FilterTask): ITasksState => {
+  return tasks.filter((task) => {
+    let textMatch: boolean = false;
+
+    if (filter.status.toLowerCase() !== "all") {
+      textMatch =
+        (task.title.toLowerCase().includes(filter.text.toLowerCase()) ||
+          task.description.toLowerCase().includes(filter.text.toLowerCase())) &&
+        task.status.toLowerCase() === filter.status.toLowerCase();
+    } else {
+      textMatch =
+        task.title.toLowerCase().includes(filter.text.toLowerCase()) ||
+        task.description.toLowerCase().includes(filter.text.toLowerCase());
+    }
+
+    return textMatch;
+  });
+};
+
+export default filterTasks;
